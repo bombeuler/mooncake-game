b20c9acb07f5c2e8b7bc76a693422daa623475d9
bomb <bombeuler@163.com>
2020-09-13 22:33:05 +0800

    安装rxjs

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import VueRx from "vue-rx";
 
 //此处引入组件
 import { Button } from "vant";
@@ -10,6 +11,8 @@ import { Form } from "vant";
 Vue.use(Form);
 Vue.use(Button);
 //引入完记得use
+
+Vue.use(VueRx);
 
 Vue.config.productionTip = false;
 
