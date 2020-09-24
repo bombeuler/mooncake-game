eab5f4e44072595a5e5e77383a3fbf85e701ff37
bomb <bombeuler@163.com>
2020-09-24 19:05:34 +0800

    游戏逻辑基本完成

--- src/untils/Student.js
+++ src/untils/Student.js
@@ -1,6 +1,6 @@
 import { shuffle } from "lodash";
-const MAX = 10;
-const MIN = 3;
+const MAX = 15;
+const MIN = 5;
 const RADNUM = 3;
 const Just0to1 = x => Math.exp(-x);
 const subLists = new Map([
@@ -13,11 +13,10 @@ const subLists = new Map([
 ]);
 class Student {
   static subLists = subLists;
-
   constructor(score = 0, isFirst = false) {
     const step = MAX - MIN;
     const num = Math.ceil(Math.random() * 3);
-    const time = (MIN + step * Just0to1(score)) * num;
+    const time = parseInt(MIN + step * Just0to1(score)) * num;
     const timePlus = isFirst ? 8 : 0;
     const randomTime =
       Math.floor(Math.random() * RADNUM) - Math.floor(Math.random() * RADNUM);
@@ -25,18 +24,39 @@ class Student {
     const preSubjects = shuffle(arr).slice(0, num);
     let preArr = [];
     for (let i = 0; i < preSubjects.length; i++) {
+      const _this = this;
       const scored = 60 + Math.floor(Math.random() * 40);
-      preArr.push({
-        subject: preSubjects[i],
-        grade: scored,
-        finnished: false,
-        eatAdd: 0
-      });
+      const handler = {
+        get(target, propKey) {
+          if (
+            propKey === "eatAdd" &&
+            target[propKey] >= target.grade &&
+            !target.finnished
+          ) {
+            target.finnished = true;
+            _this.finnishedSubjects++;
+          }
+          return target[propKey];
+        }
+      };
+      preArr.push(
+        new Proxy(
+          {
+            subject: preSubjects[i],
+            grade: scored,
+            finnished: false,
+            eatAdd: 0
+          },
+          handler
+        )
+      );
     }
+    this.subjectsNum = num;
+    this.finnishedSubjects = 0;
     this.deadSubjects = preArr;
-    this.deadTime = parseInt(time + randomTime + timePlus);
+    this.deadTime = time + randomTime + timePlus;
     this.sugarNeed = Math.ceil(Math.random() * 3);
   }
 }
 
-export default Student;
+export { Student, MIN };
--- src/views/Game.vue
+++ src/views/Game.vue
@@ -2,18 +2,51 @@
 <template>
   <div>
     <main class="game">
-      <van-row type="flex" class="top" justify="start" align="center">
-        <van-col span="8" offset="2">{{ name }}</van-col>
-        <van-col span="4" offset="1" class="my-score"
-          >{{ sugar }}+{{ dough }}</van-col
+      <van-row type="flex" class="top" justify="space-around" align="center">
+        <van-col class="user-box">
+          <van-image
+            width="5.4vw"
+            :src="require('./../assets/user_icon.png')"
+            class="icon-box"
+          />
+          <span class="user-name"> {{ name }}</span></van-col
         >
-        <van-col span="7">剩余机会：{{ score }}</van-col>
+        <van-col span="4"
+          ><div class="my-score">{{ score }}</div></van-col
+        >
+        <van-col span="8" class="life-box"
+          ><van-rate
+            icon="like"
+            size="8vw"
+            readonly
+            count="3"
+            v-model="life"
+            void-icon="like"
+            :touchable="false"
+            void-color="#dfe6e9"
+            color="#ef4971"
+        /></van-col>
       </van-row>
       <van-row class="middle">
         <div class="main-food">
-          <van-button @click="plusItem('sugar', 15)">烧糖</van-button>
-          <van-button @click="plusItem('dough', 15)">面</van-button>
-          <van-button @click="deleteSubject">垃圾桶</van-button>
+          <van-image
+            @click="plusItem('sugar', sugarMax)"
+            fit="contain"
+            width="13.8vw"
+            :src="require('../assets/sugar.png')"
+          />
+          <van-image
+            @click="plusItem('dough', doughMax)"
+            fit="contain"
+            width="15.7vw"
+            :src="require('../assets/dough.png')"
+          />
+          <van-image
+            @click="deleteSubject"
+            fit="contain"
+            width="12.3vw"
+            :src="require('../assets/trash.png')"
+          />
         </div>
         <div class="student" @click="makeCake">
           <div class="notice">{{ notice }}</div>
@@ -26,11 +59,7 @@
               v-for="sub in nowStudent.deadSubjects"
               :key="sub.subject"
               class="grade-group"
-              :background-image="
-                `linear-gradient(to right,${progressColor} ${parseInt(
-                  (sub.eatAdd / sub.grade) * 100
-                )}%, white 0px)`
-              "
+              :style="`background-image:${progressNum(sub)}`"
             >
               <span>{{ subValue(sub.subject) }}</span>
               <span>{{ sub.grade }}</span>
@@ -61,22 +90,32 @@
 </template>
 
 <script>
-import Student from "../untils/Student";
+import { Student, MIN } from "../untils/Student";
 
-const badPage = 0.05;
+const badPage = 0.02;
+const subjectMax = 5;
+const scoreStep = 10;
+const scoreExtend = 6;
 
 export default {
   name: "Game",
   data() {
     return {
       notice: "",
-      progressColor: "#e67e22",
+      progressColor: "#ecc02c",
       firstTime: null,
+      firstPeopleTime: null,
       endTime: null,
-      name: "BOMB",
-      score: 34,
+      endPeopleTime: null,
+      cleanTimeout: false,
+      name: "我是谁，8个字符",
+      life: 3,
+      isAlive: true,
+      score: 0,
       sugar: 0,
+      sugarMax: 25,
       dough: 0,
+      doughMax: 25,
       nowStudent: new Student(0, true),
       hasSubject: [],
       subjects: [
@@ -90,6 +129,12 @@ export default {
     };
   },
   computed: {
+    progressNum() {
+      return sub =>
+        `linear-gradient(to right,${this.progressColor} ${parseInt(
+          (sub.eatAdd / sub.grade) * 100
+        )}%, #ef4971 0px)`;
+    },
     subValue() {
       return subjectKey => Student.subLists.get(subjectKey);
     },
@@ -106,6 +151,17 @@ export default {
       return out;
     }
   },
+  watch: {
+    "nowStudent.finnishedSubjects": {
+      // eslint-disable-next-line no-unused-vars
+      handler(newVal, oldVal) {
+        if (newVal === this.nowStudent.subjectsNum) {
+          this.plusScore(scoreStep);
+          this.makeStudent(this.score);
+        }
+      }
+    }
+  },
   methods: {
     plusItem(item, limit) {
       if (this[item] < limit) {
@@ -116,7 +172,7 @@ export default {
       return true;
     },
     plusSubject(subkey) {
-      if (this.hasSubject.length < 3) {
+      if (this.hasSubject.length < subjectMax) {
         const random = Math.random();
         if (random <= badPage) {
           this.hasSubject.push(false);
@@ -128,13 +184,27 @@ export default {
       }
       return true;
     },
+    plusScore(step) {
+      const justTime = new Date().getTime();
+      this.endPeopleTime = justTime;
+      if (
+        justTime - this.firstPeopleTime <=
+        MIN * this.nowStudent.subjectsNum * 1000
+      ) {
+        this.score += scoreExtend;
+      }
+      this.score += step;
+    },
     deleteSubject() {
       this.hasSubject.splice(0, this.hasSubject.length);
     },
     async timeInterval(todo, time = 1, interval = 1000) {
       for (let j = 0; j < time; j++) {
-        console.log(4);
         await this.timeout(todo, j, interval);
+        if (this.cleanTimeout) {
+          this.cleanTimeout = false;
+          break;
+        }
       }
     },
     timeout(doSome, argue, time = 1000) {
@@ -152,19 +222,33 @@ export default {
         }
       });
     },
-    //FIXME 性能问题
     makeStudent(score = 0, init = false) {
       let made = {};
       if (init) {
         made = this.nowStudent;
       } else {
+        this.cleanTimeout = true;
         made = new Student(score);
+        this.firstPeopleTime = this.endPeopleTime;
         this.nowStudent = made;
-        this.deadTime = made.deadTime;
       }
+      this.deadTime = made.deadTime;
+      this.finnishedSub = 0;
       this.timeInterval(t => {
         this.notice = `还剩${(made.deadTime - t).toString()}秒`;
-      }, this.deadTime);
+        if (made.deadTime - t <= 0) {
+          console.log(t);
+          if (this.life > 0) {
+            this.life--;
+            this.plusScore(0);
+            this.makeStudent(this.score);
+          } else {
+            this.cleanTimeout = true;
+            this.endTime = new Date().getTime();
+            this.isAlive = false;
+          }
+        }
+      }, this.deadTime + 1);
     },
     diffSubject() {
       let flag = false;
@@ -172,17 +256,14 @@ export default {
         return -1;
       } else {
         const pHasSubject = this.hasSubject;
-        const pDeadSubjects = this.nowStudent.deadSubjects;
-        // eslint-disable-next-line for-direction
+        let pDeadSubjects = this.nowStudent.deadSubjects;
         for (let i = pHasSubject.length - 1; i >= 0; i--) {
           for (let j = 0; j < pDeadSubjects.length; j++) {
             if (pHasSubject[i] === pDeadSubjects[j].subject) {
               flag = [i, j];
-              console.log("yu");
               break;
             }
           }
-          //FIXME 多次
           if (flag === "boolean") {
             break;
           }
@@ -192,37 +273,42 @@ export default {
     },
     isEnough() {
       if (this.sugar >= this.nowStudent.sugarNeed && this.dough >= 1) {
-        return false;
-      } else {
         return true;
+      } else {
+        return false;
       }
     },
     makeCake() {
       //TODO 点击学生事件
-      console.log(this.diffSubject());
       if (
         this.diffSubject() !== false &&
         this.diffSubject() !== -1 &&
         this.isEnough()
       ) {
         const flg = this.diffSubject();
-        console.log(flg);
-        this.sugar -= this.nowStudent.sugarNeed;
-        this.dough--;
-        this.nowStudent.deadSubjects[flg[1]].eatAdd += 10;
-        this.hasSubject.splice(flg[0], 1);
+        if (!this.nowStudent.deadSubjects[flg[1]].finnished) {
+          this.sugar -= this.nowStudent.sugarNeed;
+          this.dough--;
+          this.nowStudent.deadSubjects[flg[1]].eatAdd += 10;
+          this.hasSubject.splice(flg[0], 1);
+        }
       }
     }
   },
   created() {},
   mounted() {
     this.firstTime = new Date().getTime();
-    this.makeStudent(true);
+    this.firstPeopleTime = this.firstTime;
+    this.makeStudent(0, true);
   }
 };
 </script>
 
 <style lang="scss" scoped>
+$birthRed: #ef4971;
+$sadBlue: #40eaff;
+$bombYellow: #ecc02c;
+
 @mixin flex-normal($direct) {
   display: flex;
   flex-direction: $direct;
@@ -251,23 +337,68 @@ export default {
   flex-direction: column;
   width: 100vw;
   height: 100vh;
+  background: {
+    image: url("../../public/bg.png");
+    repeat: no-repeat;
+    size: 100%;
+  }
   .top {
-    height: 10vh;
-    background: burlywood;
+    height: 20.4vw;
+    background: {
+      image: url("../../public/head.png");
+      repeat: no-repeat;
+      size: 100%;
+    }
     flex: {
       grow: 0;
       shrink: 0;
     }
+    .user-box {
+      @include flex-normal(row);
+      // max-width: 20vw;
+      padding: {
+        left: 5px;
+        right: 7px;
+      }
+      height: 9.7vw;
+      border-radius: 5.3vw;
+      background: $sadBlue;
+      text-align: center;
+      .icon-box {
+        flex-grow: 0;
+        flex-shrink: 0;
+      }
+      .user-name {
+        color: $birthRed;
+        white-space: nowrap;
+        word-break: keep-all;
+        padding-left: 7px;
+        font: {
+          size: 4vw;
+        }
+      }
+    }
     .my-score {
+      color: $sadBlue;
+      margin: 0 auto;
+      width: fit-content;
       font: {
-        size: 1.5em;
+        size: 8.5vw;
         family: arual;
       }
+    }
+    .life-box {
+      @include flex-normal(row);
+      flex-grow: 0;
+      flex-shrink: 0;
+      height: 9.7vw;
+      border-radius: 5.3vw;
+      background: $sadBlue;
+      text-align: center;
     }
   }
   .middle {
     @include flex-normal(row);
-    background: rgb(140, 189, 140);
     flex: {
       grow: 1;
       shrink: 0;
@@ -303,7 +434,11 @@ export default {
           width: 100%;
           @include flex-normal(row);
           user-select: none;
-          padding: 2px;
+          overflow: hidden;
+          padding: {
+            top: 2px;
+            bottom: 2px;
+          }
           @include border-normal(top);
         }
       }
@@ -312,7 +447,6 @@ export default {
   .bottom {
     height: 25vh;
     width: 100vw;
-    background: darkgrey;
     display: flex;
     justify-content: space-around;
     flex: {
